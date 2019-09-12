650481e8a0295e3915500643fd1ce6aaa8c60e6d
githubnicholas <nicholas.masse@monithq.ca>
2019-09-12 16:43:53 -0400

    first functional try

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,9 +1,30 @@
+//LAB3
+var listeFilms = null;
 
+function chargerDonnees(){
+    $.ajax({
+        url:"/data/movies.xml",
+        type:"GET",
+        dataType:"xml",
+        success: function(response){
+            listeFilms = response;
+        },
+        error: function(jqXHR, textStatus, errorThrown){
+            alert(errorThrown);
+        }
+    });
+}
+
+chargerDonnees();
+
+
+
+/// LAB2
 
 // let APIKEY = f995e37aba4da33d485f87e41759bcc6
 
 let statut = "disc";
-
+let admin = false;
 let films = [];
 
 let panierDeFilms = [];
@@ -114,15 +135,232 @@ function connectMember(){
     montrer('panier');
     cacher('connect');
     montrer('disconnect');
-    clearMovies();
-    processURL(url2, instructions2);
+    showAllMovies();
+    if (admin){
+        cacher('myMovies');
+        cacher('panier');
+        cacher('navbarDropdown');
+        montrer('addMovie');
+    }
+}
+///
+
+
+
+
+// LAB3
+
+function updateMovie(){
+    if (document.getElementById('modifTitre').value != "" &
+    document.getElementById('modifID').value != "" &
+    document.getElementById('modifImage').value != "" &
+    document.getElementById('modifDate').value != ""){
+        cacher('alertModif');
+        $('#modalModif').modal('hide');
+        //
+        let results = listeFilms.getElementsByTagName("results");
+        let elementToUpdate = null;
+        let idToUpdate = document.getElementById('modifID').value;
+        for (let x = 0;x < results.length;x++){
+            if (results[x].getElementsByTagName('id')[0].firstChild.nodeValue == idToUpdate){
+                 elementToUpdate = results[x];
+            }
+        }
+        elementToUpdate.getElementsByTagName('title')[0].firstChild.nodeValue = document.getElementById('modifTitre').value;
+        elementToUpdate.getElementsByTagName('poster_path')[0].firstChild.nodeValue = document.getElementById('modifImage').value;
+        elementToUpdate.getElementsByTagName('release_date')[0].firstChild.nodeValue = document.getElementById('modifDate').value;
+        if (elementToUpdate.getElementsByTagName('overview')[0].firstChild != null){
+            elementToUpdate.getElementsByTagName('overview')[0].firstChild.nodeValue = document.getElementById('modifResume').value;
+        }else{
+            text = document.createTextNode(document.getElementById('modifResume').value);
+            elementToUpdate.getElementsByTagName('overview')[0].appendChild(text);
+        }
+
+        showAllMovies();
+   
+        document.getElementById('modifID').disabled = false;
+
+
+    }else{
+        montrer('alertModif');
+    }
+}
+
+function addMovieToXML(){
+    if (document.getElementById('ajoutTitre').value != "" &
+    document.getElementById('ajoutID').value != "" &
+    document.getElementById('ajoutImage').value != "" &
+    document.getElementById('ajoutDate').value != ""){
+        cacher('alertAjout');
+        $('#modalAjout').modal('hide');
+        //
+        newMovie = document.createElement('results');
+        
+        node = document.createElement('title');
+        text = document.createTextNode(document.getElementById('ajoutTitre').value);
+        node.appendChild(text);
+        newMovie.appendChild(node);
+
+        node = document.createElement('id');
+        text = document.createTextNode(document.getElementById('ajoutID').value);
+        node.appendChild(text);
+        newMovie.appendChild(node);
+
+        node = document.createElement('poster_path');
+        text = document.createTextNode(document.getElementById('ajoutImage').value);
+        node.appendChild(text);
+        newMovie.appendChild(node);
+
+        node = document.createElement('backdrop_path');
+        text = document.createTextNode(document.getElementById('ajoutImage').value);
+        node.appendChild(text);
+        newMovie.appendChild(node);
+
+        node = document.createElement('release_date');
+        text = document.createTextNode(document.getElementById('ajoutDate').value);
+        node.appendChild(text);
+        newMovie.appendChild(node);
+
+        node = document.createElement('overview');
+        text = document.createTextNode(document.getElementById('ajoutResume').value);
+        node.appendChild(text);
+        newMovie.appendChild(node);
+
+        listeFilms.firstChild.appendChild(newMovie);
+
+        showAllMovies();
+
+        
+
+
+
+
+    }else{
+        montrer('alertAjout');
+    }
+
+}
+
+function removeMovie(){
+    let element = event.target;
+    let idToDelete = element.parentElement.parentElement.getAttribute('movieID');
+    let results = listeFilms.getElementsByTagName("results");
+    let elementToDelete = null;
+    for (let x = 0;x < results.length;x++){
+        if (results[x].getElementsByTagName('id')[0].firstChild.nodeValue == idToDelete){
+            elementToDelete = results[x];
+            elementToDelete.parentElement.removeChild(elementToDelete);
+        }
+    }
+    showAllMovies();
 }
 
 function showAllMovies(){
     clearMovies();
-    processURL(url2, instructions2);
+    let results = listeFilms.getElementsByTagName("results");
+    Array.from(results).forEach(element => {
+        let card = document.createElement('div');
+        card.classList.add('card');
+        let att = document.createAttribute('movieID');
+        att.value = element.getElementsByTagName('id')[0].firstChild.nodeValue;
+        card.setAttributeNode(att);
+        let img = document.createElement('img');
+        img.classList.add('card-img-top');
+        img.alt = element.getElementsByTagName('backdrop_path')[0].firstChild.nodeValue;
+        if (element.getElementsByTagName('poster_path')[0].firstChild.nodeValue.includes('http')){
+            img.src = element.getElementsByTagName('poster_path')[0].firstChild.nodeValue;
+        }else{
+            img.src = "https://image.tmdb.org/t/p/w500/" + element.getElementsByTagName('poster_path')[0].firstChild.nodeValue.substring(1);
+        }
+        img.setAttribute('data-toggle', 'modal');
+        img.setAttribute('data-target', '#modalTrailer');
+        img.addEventListener('click', showTrailer);
+        card.appendChild(img);
+        let cardbody = document.createElement('div');
+        cardbody.classList.add('card-body');
+        cardbody.classList.add('d-flex');
+        cardbody.classList.add('flex-column');
+        let title = document.createElement('h5');
+        title.classList.add('card-title');
+        title.innerHTML = element.getElementsByTagName('title')[0].firstChild.nodeValue;
+        cardbody.appendChild(title);
+        let desc = document.createElement('p');
+        desc.classList.add('card-text');
+        let description = "";
+        if (element.getElementsByTagName('overview')[0].firstChild != null){
+            description = element.getElementsByTagName('overview')[0].firstChild.nodeValue;
+        }
+        desc.innerHTML = description;
+        cardbody.appendChild(desc);
+        let date = document.createElement('p');
+        date.classList.add('card-text');
+        let datetext = document.createElement('small');
+        datetext.classList.add('text-muted');
+        datetext.innerHTML = "Date de sortie : " + element.getElementsByTagName('release_date')[0].firstChild.nodeValue;
+        date.appendChild(datetext);
+        cardbody.appendChild(date);
+        if (statut == "conn" & !admin){
+            let button = document.createElement('button');
+            button.classList.add('btn');
+            button.classList.add('btn-success');
+            button.classList.add('mt-auto');
+            button.type = "button";
+            button.innerHTML = "Ajouter au panier";
+            button.addEventListener('click', addMovie);
+            cardbody.appendChild(button);
+        }
+        if(admin){
+            let button2 = document.createElement('button');
+            button2.classList.add('btn');
+            button2.classList.add('btn-info');
+            button2.classList.add('mb-1');
+            button2.classList.add('mt-auto');
+            button2.type = "button";
+            button2.innerHTML = "Modifier le film";
+            button2.addEventListener('click', montrerModif);
+            cardbody.appendChild(button2);
+            //
+            let button = document.createElement('button');
+            button.classList.add('btn');
+            button.classList.add('btn-danger');
+            button.type = "button";
+            button.innerHTML = "Supprimer le film";
+            button.addEventListener('click', removeMovie);
+            cardbody.appendChild(button);
+            
+        }
+        card.appendChild(cardbody);
+        let container = document.getElementById('deck');
+        container.appendChild(card);
+        
+    });
 }
 
+function montrerModif(){
+    let element = event.target;
+    let idToDelete = element.parentElement.parentElement.getAttribute('movieID');
+    let results = listeFilms.getElementsByTagName("results");
+    let elementToUpdate = null;
+    for (let x = 0;x < results.length;x++){
+        if (results[x].getElementsByTagName('id')[0].firstChild.nodeValue == idToDelete){
+            elementToUpdate = results[x];
+        }
+    }
+    document.getElementById('modifTitre').value = elementToUpdate.getElementsByTagName('title')[0].firstChild.nodeValue;
+    document.getElementById('modifID').value = elementToUpdate.getElementsByTagName('id')[0].firstChild.nodeValue;
+    document.getElementById('modifID').disabled = true;
+    document.getElementById('modifImage').value = elementToUpdate.getElementsByTagName('poster_path')[0].firstChild.nodeValue;
+    document.getElementById('modifDate').value = elementToUpdate.getElementsByTagName('release_date')[0].firstChild.nodeValue;
+    if (elementToUpdate.getElementsByTagName('overview')[0].firstChild != null){
+        document.getElementById('modifResume').value = elementToUpdate.getElementsByTagName('overview')[0].firstChild.nodeValue;
+    }
+    $('#modalModif').modal('toggle');
+}
+
+
+
+
+////////////////////////////////////////
 function disconnect(){
     statut = "disc";
     cacher('myMovies');
@@ -130,8 +368,11 @@ function disconnect(){
     cacher('panier');
     montrer('connect');
     cacher('disconnect');
-    clearMovies();
-    processURL(url2, instructions2);
+    if (admin){
+        admin = false;
+        cacher('addMovie');
+    }
+    showAllMovies();
 }
 
 function vider(id){
@@ -146,6 +387,9 @@ function connexion(nom, mdp){
     liste.forEach(element => {
         if ((element['nom'] == nom) & (element['mdp'] == mdp)){
             valide = true;
+            if (nom == 'admin' & mdp == 'admin'){
+                admin = true;
+            }
         }
     });
     
@@ -313,6 +557,7 @@ let request1 = [url1, instructions1];
 
 
 
+
 let url2 = "https://api.themoviedb.org/3/discover/movie?api_key=f995e37aba4da33d485f87e41759bcc6&language=fr-CA&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
 instructions2 = function(){
     let moviesData = JSON.parse(this.response);
@@ -372,9 +617,11 @@ let request2 = [url2, instructions2]
 
 let requests = [request1, request2];
 
+
 requests.forEach(element => {
-    processURL(element[0], element[1]);
-});
+processURL(element[0], element[1]);});
+
+
 
 
 
